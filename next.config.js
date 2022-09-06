72fe310eb4c442a9a2eb347e00fec1258b1bb646
Sahil Maheshwari <sahilm1711@gmail.com>
2022-09-06 13:51:07 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env:{
-    API_KEY:'ffjJwK5ZijCNygN6TGiKPCGtmbtY2BHCs4dyasCgtrrtzaVd8METfoIN8t3KBxIO'
-  },
 }
 
 module.exports = nextConfig
